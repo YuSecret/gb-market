876e4e32a735fe317d5648664ceca0f15517701a
Yury <4sovremennik@gmail.com>
2020-09-20 18:04:53 +0300

    Initial commit

---
+++ src/main/java/com/gb/market/gbmarket/GbMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gb.market.gbmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gb/market/gbmarket/GbMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.market.gbmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
